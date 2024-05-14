67c8c4f3e892f61cd2bda0210ec655b24bff6bbd
Florian Schmidt <schmidt.florian90@gmail.com>
2024-05-14 02:44:34 +0200

    baseurl changed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://documentation.finalfactory.de',
+  url: 'https://docs.finalfactory.de',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/', //documentaton/final-preferences
+  baseUrl: '/documentation',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
